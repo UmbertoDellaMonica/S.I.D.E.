3014cd772130c34e5cf498f24796f202b5c1979f
Umberto Della Monica <umberto.dellamonica@hotmail.it>
2025-09-15 12:17:11 +0200

    update route
    - dynamic api route
    - dynamic route front-end
    - Enum role

--- side-frontend/app/lib/dynamic.router.ts
+++ side-frontend/app/lib/dynamic.router.ts
@@ -5,32 +5,6 @@ export const ROUTES = {
   home: "/",
   back: "/",
 
-  // Auth Routes
-  login: "/auth/login",
-  signup: "/auth/signup",
-
-  // Customer Routes
-  customerHome: "/customer",
-  customerProfile: "/customer/profile",
-  customerFeedback: "/customer/feedback",
-  customerRequests: "/customer/requests",
-
-  // Business Routes
-  business: "/business",
-  businessProfile: "/business/profile",
-  businessDashboard: "/business/dashboard",
-  businessCustomers: "/business/customers",
-  businessReservations: "/business/reservations",
-  businessFeedbacks: "/business/feedbacks",
-  businessStaff: "/business/staff",
-  businessSettings: "/business/settings",
-
-  // Admin Routes
-  adminLogin: "/admin/login",
-  adminDashboard: "/admin/dashboard",
-  adminCustomers: "/admin/customers",
-  adminSettings: "/admin/settings",
-  adminBusiness: "/admin/business",
-  adminBannerCustomer: "/admin/banner/customer",
-  adminBannerBusiness: "/admin/banner/business",
+  /// Dashboard - route
+  dashboard: "/dashboard",
 };
